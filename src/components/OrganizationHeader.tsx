287821be9d2461832b42e0e9380bc8681315611a
horsefacts <webmaster@terminally.online>
2025-04-04 15:19:43 -0400

    update embed image

--- src/components/OrganizationHeader.tsx
+++ src/components/OrganizationHeader.tsx
@@ -51,8 +51,8 @@ export function OrganizationHeader({ cause, onInfoClick }: OrganizationHeaderPro
                 <p className="text-base text-white/75 font-light">{cause.shortDescription}</p>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
+                  width="14"
+                  height="14"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
